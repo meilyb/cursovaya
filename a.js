bcd20f3b09dd64d84c42811eea06534e58ad3bda
Sonya0904 <soph.zolotaryova01@gmail.com>
2026-02-22 19:25:09 +0300

    верх готов

--- a.js
+++ a.js
@@ -1,3 +1,29 @@
-function tap(){
-    
+$("[data-modal]").click(function(){
+let modalID=$(this).data("modal");
+$(`#${modalID}`).addClass("active");
+});
+function check(){
+  
+    if(document.forms.login.elements.Login.value==""){
+        alert('заполните логин');
+    }
+    if(document.forms.login.elements.emaill.value==""){
+            alert('заполните почту')
+    }
+        if(document.forms.login.elements.pass.value==""){
+            alert('заполните пароль')
+    }
+       if(document.forms.login.elements.pass.value<7){
+            alert('пароль должен содержать не менее 7 символов')
+    }
+    if(document.forms.login.elements.Login.value!=""&&document.forms.login.elements.pass.value!=""&&document.forms.login.elements.emaill.value!=""){
+    alert(`на почту ${email} отправлено письмо с подтверждением`)
+    }
+}
+$("[data-close]").click(function(){
+let modalID=$(this).data("close");
+$(`#${modalID}`).removeClass("active");
+});
+function drop() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
